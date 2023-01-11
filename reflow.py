43acc6363beef70b0eaa44948ffc5cb7cb1744d2
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2023-01-11 07:33:37 -0500

    rm multiline

--- reflow.py
+++ reflow.py
@@ -4,18 +4,14 @@ import textwrap
 def reflow(content: str, width: int = 80, indent: int = 0, comments: str = "") -> str:
     content_lines = []
     for line in content.splitlines():
-        if line.isspace():
-            content_lines.append(line)
-            continue
-        if comments != "":
-            # don't use replace(),
-            # as that will remove non-leading characters in this string
+        if comments.strip() != "":
+            # don't use replace(), as that will remove non-leading characters
+            # in this string
             line = line.lstrip().lstrip(comments).lstrip()
         content_lines.append(line.strip() + " ")
     text = "".join(content_lines)
     comment_width = 0 if comments == "" else len(comments) + 1
     preceeding_width = indent + comment_width
-    # text = " ".join(l.strip() for l in content.splitlines() if l.isspace())
     text_lines = textwrap.wrap(text, width - preceeding_width)
     if comments != "":
         text_lines = [f"{comments} {line}" for line in text_lines]
